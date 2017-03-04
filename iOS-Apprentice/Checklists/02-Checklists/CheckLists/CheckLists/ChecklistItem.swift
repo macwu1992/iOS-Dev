0a1eb07a53659cbd4be854c2b24b9f79e967d2c2
mawu1992 <mawu1992.hotmail.com>
2017-03-04 15:25:45 +0800

    add button and delete items func complete

---
+++ iOS-Apprentice/Checklists/02-Checklists/CheckLists/CheckLists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  CheckLists
+//
+//  Created by 吴童 on 2017/3/4.
+//  Copyright © 2017年 吴童. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var isChecked = false
+}
